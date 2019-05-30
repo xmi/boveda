fbf505da25104877ef0c2dc73fd3389ba0003320
eonmilu <eonmilu@gmail.com>
2019-05-31 01:20:07 +0200

    Move web files to web/, serve it

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ var express = require("express")
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const fallbackPath = "./fallback.png";
+const fallbackPath = "./web/fallback.png";
 
 var app = express();
 
-app.get("/", function(req, res) {
-	res.send("Hello world");
-});
+app.use(express.static("web"));
 
 app.listen(port);
 console.log("API on " + port);
