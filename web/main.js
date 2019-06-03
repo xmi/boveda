b3fcc5a87e4e55d11531e63ab3cfa659aaf996d3
eonmilu <eonmilu@gmail.com>
2019-06-03 02:04:04 +0200

    Skeleton

---
+++ web/main.js
@@ -0,0 +1,19 @@
+var config;
+// config.malditaPerImage: aprox maldita articles to be shown per image
+
+const configRefreshInterval = 5 * 60 * 1000;
+setInterval(() => {
+    $.getJSON("get/web", function (data) {
+        config = data;
+    });
+}, configRefreshInterval);
+
+
+setInterval(() => {
+    if (Math.random() > config.malditaPerImage) {
+        //show image
+        
+    } else {
+        //show article
+    }
+}, config.cycleInterval);
