7387c32b686043abf9d33a1cde75093fc70e6156
eonmilu <eonmilu@gmail.com>
2019-06-20 02:42:31 +0200

    Restructure

--- web/main.js
+++ web/main.js
@@ -23,28 +23,27 @@ $.getJSON("config.json", function (data) {
 
 
     while (true) {
+
         // counter to force browser to ignore cache
         $("iframe").attr("src", `/get/photo?${counter++}`);
 
-        // if length > 0 there is a video tag
+        //wait for media to load
+        await sleep(500);
+
+        var wait;
+        // unify video and image
         if ($("#bg").contents().find("video").length > 0) {
-            // Change the html in the iframe to ccenter the video
-            $("#bg").contents().find("video").wrap('<div style="display:flex;justify-content:center;align-items:center;"> </div>');
-
-            // set autoplay
-            $("#bg").contents().find("video").attr("autoplay", "true");
-
-            var videoLength = document.getElementsByTagName("video")[0].duration;
-            await sleep(videoLength);
-        } else if ($("#bg").contents().find("img").length > 0) { // if length > 0 there is a img tag
-            // Change the html in the iframe to ccenter the image
-            $("#bg").contents().find("img").wrap('<div style="display:flex;justify-content:center;align-items:center;"> </div>')
-
-            await sleep(config.cycleInterval);
+            $("#bg").contents().find("video").attr("id", "media");
+            $("#media", $("#bg").contents()).get(0).play();
+            wait = $("#media", $("#bg").contents()).get(0).duration * 1000;
         } else {
-            console.log("no image or video found")
+            $("#bg").contents().find("img").attr("id", "media");
+            wait = config.cycleInterval;
         }
 
+        await sleep(wait);
+        // wait a bit to not overload browsers
+        await sleep(200);
     }
 }).fail(() => {
     console.log("Cant get config")
